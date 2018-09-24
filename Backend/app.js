92d5a4c1f0108939cd02ca6ada009700f6311de2
John Alden <jzalden96@gmail.com>
2018-09-23 17:04:17 -0700

    commit intial files
    Some of the files are only included for temporary purposes a and will be removed before pulling

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,18 @@
-//plugins
-const express = require('express')
+//modules
+const express    = require('express');
+const bodyParser = require('body-parser');
 
-//definitions
-const app = express()
-const port = 3300
+//localfiles
+const auth       = require('./login.js')
+
+//module setup
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//constants
+const port = 3300;
 
 //start server
 app.listen(port, () => {
@@ -12,6 +21,29 @@ app.listen(port, () => {
 
 //path listeners
 app.get('/', function (req, res) {
-  res.send('<p style="color: red;">Welcome to Cork\'d! Coming Soon!</p>')
-})
+    res.sendFile("login.html", { root: __dirname });
+});
 
+app.post('/api/login', function (req, res) {
+    let body = req.body;
+
+    let login_info = auth.login(body.email, body.password);
+
+    if(login_info != null) {
+        res.send(login_info); //TODO: does more info need to be sent?
+    }
+
+    res.statusCode = 401
+    res.send("")
+});
+
+app.post('/api/register', function (req, res) {
+    let body = req.body;
+
+    if(auth.register(body.email, body.password)) {
+        res.statusCode = 200;
+    }
+
+    res.statusCode = 401;
+    res.send("")
+});
