40a957c1080d7c8d174086f0f6f563f65756029a
Alireza Bahremand <abahrema@asu.edu>
2018-10-01 18:25:17 -0700

    React Skeleton Component Structure
    Using team-discussed class structure for creating react components representing UI wireframe.

---
+++ Frontend/components/sidebar/TabbedPanels.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class TabbedPanels extends React.Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (<div><p>The tab panel</p></div>);
+    }
+}
+
+export default TabbedPanels;
