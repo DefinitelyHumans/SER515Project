e7b11b8c624f7c27bbc4dbe3860e1a0fcd7cc2e7
Cecilia <laplace95@gmail.com>
2018-10-15 17:19:54 -0700

    US39T48-Updating current state to go fix merge conflicts in Namratha's PR

---
+++ Frontend/src/sidebar/menu/subscriptions/subscriptionCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SubscriptionCard extends React.Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (<div><p>This is the subscription panel</p></div>);
+    }
+}
+
+export default SubscriptionCard;
---
+++ Frontend/src/sidebar/menu/topics/topicCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class TopicCard extends React.Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (<div><p>This is the topic card panel</p></div>);
+    }
+}
+
+export default TopicCard;
