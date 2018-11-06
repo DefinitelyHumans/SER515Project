ddd28fb72da87778c7e9c9bd63b2eb14942eed2f
John Alden <jzalden96@gmail.com>
2018-11-06 15:36:13 -0700

    convert topic endpoints to process JWT's

--- Backend/routes/topic_routes.js
+++ Backend/routes/topic_routes.js
@@ -10,7 +10,7 @@ var router = express.Router()
 
 // TOPIC ROUTES
 router.get('/:id', (req, res) => {
-    let id = req.topic_id;
+    let id = req.params.id;
 
     topic.GetTopic(id)
         .then((topic) => {
@@ -31,22 +31,19 @@ router.get('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-    if (req.body.user_id == undefined) {res.status(400).send("Invalid User ID.");}
-
     let id = req.params.id;
-    let token = req.body.token;
-    let userID = req.body.user_id;
+    let user_id;
+    if(req.token) user_id = token.check_token(req.token);
+    else res.status(401).send("Authentication token required");
 
-    if (!ObjectID.isValid(id)) {
-        return res.status(404).send();
-    }
+    if(!user_id) res.status(401).send("Invalid authentication token");
 
     topic.DeleteTopic(userID, id, token)
         .then((topic) => {
             if (!topic) {
-                return res.status(404).send();
+                res.status(404).send();
             }
-            return res.status(200).send();
+            res.status(200).send();
         })
         .catch((e) => {
             res.status(500).send();
@@ -55,14 +52,15 @@ router.delete('/:id', (req, res) => {
 
 // Updating existing topic
 router.put('/:id',(req,res)=>{
-    if (req.body.user_id == undefined) {res.status(400).send("Invalid User ID.");}
-
+    let body = req.body;
     let id = req.params.id;
-    let token = req.body.token;
-    let content = req.body.content;
-    let userID = req.body.user_id;
+    let user_id;
+    if(req.token) user_id = token.check_token(req.token);
+    else res.status(401).send("Authentication token required");
 
-    topic.UpdateTopic(userID, id,token,content)
+    if(!user_id) res.status(401).send("Invalid authentication token");
+
+    topic.UpdateTopic(user_id, id, token, content)
         .then((topic)=>{
             res.send({
                 topic_time_posted : topic.update_time,
@@ -73,17 +71,13 @@ router.put('/:id',(req,res)=>{
         });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res) => {s
     let id = req.params.id;
-    let token = req.params.token;
+    let user_id;
+    if(req.token) user_id = token.check_token(req.token);
+    else res.status(401).send("Authentication token required");
 
-    if (!ObjectID.isValid(id)) {
-        return res.status(404).send();
-    }
-
-    if (!ObjectID.isValid(id)) {  //ObjectID from postgres module
-        return res.status(404).send();
-    }
+    if(!user_id) res.status(401).send("Invalid authentication token");
 
     topic.DeleteTopic({
         _id: id,
@@ -102,15 +96,17 @@ router.delete('/:id', (req, res) => {
 
 // Creating/Posting new topic
 router.post('/create', (req,res) => {
-    if (req.body.user_id == undefined) {res.status(400).send("Invalid User ID.");}
+    let user_id;
+    if(req.token) user_id = token.check_token(req.token);
+    else res.status(401).send("Authentication token required");
+
+    if(!user_id) res.status(401).send("Invalid authentication token");
 
     let title = req.body.title;
     let type = req.body.type;
     let content = req.body.content;
-    let token = req.body.token;
-    let userID = req.body.user_id;
 
-    topic.CreateTopic(userID, title,type,content,token)
+    topic.CreateTopic(user_id, title, type, content, token)
         .then((topic)=>{
             res.send({
                 topic_id : topic.topic_id,
