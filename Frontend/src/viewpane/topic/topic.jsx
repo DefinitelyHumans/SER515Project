99abd327b99a17e6fd2bb10f977d0c413b752799
Kaelan <kstrones@asu.edu>
2018-11-19 13:07:03 -0700

    Add the delete topic functionality
    The current functionality deletes the topic from the local data and does not have a connection to the API
    
    Issue: US85_T98

--- Frontend/src/viewpane/topic/topic.jsx
+++ Frontend/src/viewpane/topic/topic.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Modal from '../../modal';
+
 
 class Topic extends React.Component {
 
@@ -6,12 +8,15 @@ class Topic extends React.Component {
         super(props);
         this.state = {
             newComment: {},
-            visable: false
+            visible: false
         }
         this.swapTopic = this.swapTopic.bind(this);
         this.renderTopicCard = this.renderTopicCard.bind(this);
         this.renderFullTopic = this.renderFullTopic.bind(this);
         this.renderComments = this.renderComments.bind(this);
+        this.showModal = this.showModal.bind(this);
+        this.hideModal = this.hideModal.bind(this);
+        this.deleteTopic = this.deleteTopic.bind(this);
     }
 
     swapTopic(){
@@ -54,6 +59,18 @@ class Topic extends React.Component {
             this.setState({newComment: {'id': this.props.topic['title'], 'user': 'user', 'time': 'time', 'content': event.target.value}});
     }
 
+    deleteTopic () {
+        this.props.parentDeleteTopic(this.props.topic);
+        this.swapTopic();
+    }
+
+    showModal () {
+        this.setState({visible: true});
+    }
+    hideModal () {
+        this.setState({visible: false});
+    }
+
     renderFullTopic(){
         return (
             <div className="FullTopic">
@@ -69,9 +86,9 @@ class Topic extends React.Component {
                         <h1 className="TopicTitle">{this.props.topic['title']}</h1>
                         <p className="TopicDescription">{this.props.topic['descrip']}</p>
                         <div className="TopicControl">
-                            <button className="TopicDelButton">Like</button>
-                            <button className="TopicDelButton">Dislike</button>
-                            <button className="TopicDelButton">Delete</button>
+                            <button className="TopicMoodButton">Like</button>
+                            <button className="TopicMoodButton">Dislike</button>
+                            <button className="TopicDelButton" onClick={this.showModal}>Delete</button>
                         </div>
                     </div>
                 </div>
@@ -83,6 +100,14 @@ class Topic extends React.Component {
                         <button type='button' onClick={this.handleSubmit.bind(this)} className="SubmitButton">Submit</button>
                     </form>
                 </div>
+                <Modal visible={this.state.visible}>
+                    <h3 className="dialogTitle">Deleting a topic</h3>
+                    <form> 
+                        <p>Are you sure you want to delete this topic?</p>
+                    </form>
+                    <button onClick={this.deleteTopic} type="button" className="closeDialogButton">Yes</button>
+                    <button onClick={this.hideModal} type="button" className="closeDialogButton">No</button>
+                </Modal>
             </div>
         );
     }
